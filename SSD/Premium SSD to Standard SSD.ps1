46b0f4e5b2464118992f4dc0a8cfd98f0fe8c4f3
derryl-johnson <derryljohnson2@gmail.com>
2024-06-12 23:15:33 -0500

    Add files via upload

---
+++ SSD/Premium SSD to Standard SSD.ps1
@@ -0,0 +1,25 @@
+﻿$AZdisks = Import-Csv -Path 'C:\Premium SSD to Standard SSD\disk_test.csv'
+            
+foreach ($AZdisk in $AZdisks)            
+{            
+    $diskname = $AZdisk.Name
+    $resourcegroup = $AZdisk.RG
+    $storageType = 'Premium_LRS'
+    $vmname = $AZdisk.owner
+
+#Gather disk info
+$disk = Get-AzDisk -DiskName $diskname -ResourceGroupName $resourcegroup
+
+
+                
+# Stop and deallocate the VM before changing the storage type
+#Stop-AzVM $resourcegroup -Name $vmname -Force
+
+# Update the storage type
+$disk.Sku = [Microsoft.Azure.Management.Compute.Models.DiskSku]::new($storageType)
+$disk | Update-AzDisk
+
+#Start-AzVM -ResourceGroupName $resourcegroup -Name $vmname   
+
+}
+
