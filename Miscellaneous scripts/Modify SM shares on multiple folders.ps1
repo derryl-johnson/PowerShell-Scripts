1395441dbe3eede1212c63d90a4a9a97329dee1a
derryl-johnson <derryljohnson2@gmail.com>
2024-06-13 10:02:23 -0500

    Add files via upload

---
+++ Miscellaneous scripts/Modify SM shares on multiple folders.ps1
@@ -0,0 +1,12 @@
+﻿$Folder = Get-ChildItem -Path C:\Web
+foreach ($Folder in $Folder)            
+{            
+    Grant-smbShareAccess -name "$Folder" -AccountName "Derryljohnson\Domain Users" -AccessRight Full -force
+    Revoke-smbshareaccess -name "$Folder" -AccountName "Everyone" -force 
+    
+
+    #This will give the Everyone group ful access again
+    #Grant-smbShareAccess -name "$Folder" -AccountName "Everyone" -AccessRight Full -force Out-Null
+    #Revoke-smbshareaccess -name "$Folder" -AccountName "Derryljohnson\Domain Users" -force Out-Null
+
+}
---
+++ Miscellaneous scripts/crmas-fs (BJ).ps1
@@ -0,0 +1,7 @@
+﻿$connectTestResult = Test-NetConnection -ComputerName crmasa.file.core.windows.net -Port 445
+if ($connectTestResult.TcpTestSucceeded) {
+    # Mount the drive
+    New-PSDrive -Name Z -PSProvider FileSystem -Root "\\crmasa.file.core.windows.net\crma-fs" -Persist
+} else {
+    Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
+}
