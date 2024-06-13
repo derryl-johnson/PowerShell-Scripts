095b34b8a976f2b1078bef9e421765dae435842a
derryl-johnson <derryljohnson2@gmail.com>
2024-06-13 09:58:48 -0500

    Add files via upload

---
+++ Change shared permission on multiple folders/Shared permissions.ps1
@@ -0,0 +1,14 @@
+﻿$Shares = Import-Csv -Path "C:\temp\planetariumfs.csv"            
+foreach ($Shares in $Shares)            
+{            
+    $foldername = $Shares.'Name'
+    $account = $Shares.'AccountName'
+    $access = $Shares.'AccessRight'
+
+    #This will revoke all permissions for all shared folders
+    #Revoke-smbshareaccess -name "$foldername" -AccountName $account -force     
+    
+    #This will restore all permissions for all shared folders
+    Grant-smbShareAccess -name "$foldername" -AccountName $account -AccessRight $access -force 
+
+}
